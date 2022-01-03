d7b02704f58592d76512c991e984ad0f32fa2bb4
fintis00 <finn.tissen@alumni.fh-aachen.de>
2022-01-03 15:01:49 +0100

    PauseMenu

---
+++ Prototyp_Kugelspiel/Assets/Pausemenu.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausemenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject PauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+}
