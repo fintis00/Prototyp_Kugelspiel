d33edb675216c54853ea4d3a6c882ae9a24e5823
fintis00 <finn.tissen@alumni.fh-aachen.de>
2022-01-04 16:49:05 +0100

    Pause jetzt aber wirklich

--- Prototyp_Kugelspiel/Assets/Pausemenu.cs
+++ Prototyp_Kugelspiel/Assets/Pausemenu.cs
@@ -11,7 +11,7 @@ public class Pausemenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             
             Pause();
