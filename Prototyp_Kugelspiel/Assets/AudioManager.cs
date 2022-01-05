6efe707756e15f3ae75cd9204fb86d46cf1116c2
fintis00 <finn.tissen@alumni.fh-aachen.de>
2022-01-05 12:45:43 +0100

    .

--- Prototyp_Kugelspiel/Assets/AudioManager.cs
+++ Prototyp_Kugelspiel/Assets/AudioManager.cs
@@ -6,8 +6,18 @@ public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
 
+    public static AudioManager instance;
+
     private void Awake()
     {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
         foreach(Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
@@ -18,14 +28,20 @@ public class AudioManager : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        Play("BackgroundAmbience");
+    }
+
     public void Play(string name)
     {
         Sound s = Array.Find(sounds, sound => sound.name == name);
         if(s == null)
         {
-            Debug.Log("Sound: " + name + " not found");
+            Debug.LogWarning("Sound: " + name + " not found");
             return;
         }
+        Debug.Log("Sound: "+ name + " is playing");
         s.source.Play();
     }
 }
