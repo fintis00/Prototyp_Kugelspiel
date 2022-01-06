ecf6beb3def69bb3e1fa28340db0fe7e30bbb74b
fintis00 <finn.tissen@alumni.fh-aachen.de>
2022-01-06 13:00:31 +0100

    settings

--- Prototyp_Kugelspiel/Assets/Scripts/Pausemenu.cs
+++ Prototyp_Kugelspiel/Assets/Scripts/Pausemenu.cs
@@ -11,7 +11,7 @@ public class Pausemenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape)&&!gameIsPaused)
         {
             
             Pause();
--- Prototyp_Kugelspiel/Assets/Scripts/SettingsController.cs
+++ Prototyp_Kugelspiel/Assets/Scripts/SettingsController.cs
@@ -1,35 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System;
 
 
 public class SettingsController : MonoBehaviour
 {
-
-    public static bool settingsAreOpen = false;
-    public GameObject SettingsUI;
-    public GameObject PauseMenuUI;
-    public GameObject slider;
+    public bool s_are_open=false;
+    [SerializeField]
+    private GameObject SettingsUI;
+    [SerializeField]
+    private GameObject PauseMenuUI;
+    [SerializeField]
+    private Slider slider;
+    [SerializeField]
+    private Text volume_text;
     private float volume;
+    private float orig_volume;
+    private Sound s;
 
-    
 
-    // Update is called once per frame
-    void changeVolume(float vol)
+    private void Start()
     {
-        
+        s_are_open = true;
+        Sound[] temp = FindObjectOfType<AudioManager>().sounds;
+        s = Array.Find(temp, sound => (sound.name).Equals("BackgroundAmbience"));
+        slider.value = s.volume;
+        orig_volume = s.volume;
+    }
+    // Update is called once per frame
+    public void changeVolume(float vol)
+    {
         volume = vol;
+        volume_text.text = ((int)(volume*100)).ToString() + "%";
+        
 
     }
 
     public void zurueck()
     {
-        Sound[] temp = FindObjectOfType<AudioManager>().sounds;
-        foreach(Sound sound in temp)
-        {
-            sound.volume=volume;
-        }
+        s_are_open= false;
+        s.volume = volume;
         SettingsUI.SetActive(false);
         PauseMenuUI.SetActive(true);
     }
