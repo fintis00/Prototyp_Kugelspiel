4a1b79e9b0d7c99f993fb5fb1e79f6306c9e8a91
fintis00 <finn.tissen@alumni.fh-aachen.de>
2022-03-03 15:04:31 +0100

    checkpoints mit particle

--- Prototyp_Kugelspiel/Assets/Scripts/checkPoint.cs
+++ Prototyp_Kugelspiel/Assets/Scripts/checkPoint.cs
@@ -6,12 +6,15 @@ public class checkPoint : MonoBehaviour
 {
     public GameObject current_checkpoint;
     public GameObject respawnpoint;
+    public ParticleSystem particleSystem;
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             current_checkpoint.transform.position = respawnpoint.transform.position;
             Destroy(gameObject);
+            var em = particleSystem.emission;
+            em.enabled = true;
         }
     }
 }
